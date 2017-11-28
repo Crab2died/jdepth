c7b902e59004994c50b4301f9a8c54c09e1b4110
Crab2Died <hwb9588@126.com>
2017-11-28 14:38:45 +0800

    concurrent

---
+++ src/main/java/com/github/jvm/concurrent/VectorConcurrentTest.java
@@ -0,0 +1,39 @@
+package com.github.jvm.concurrent;
+
+import java.util.Vector;
+
+/**
+ *  Vector 非绝对线程安全
+ *
+ */
+public class VectorConcurrentTest {
+
+    private static Vector<Integer> vector = new Vector<>();
+
+    public static void main(String[] args){
+        for (;;){
+            for(int i = 0; i < 10; i++){
+                vector.add(i);
+            }
+            Thread thread1 = new Thread(() -> {
+               // synchronized (vector){
+                    for(int i = 0; i < vector.size(); i++){
+                        vector.remove(i);
+                    }
+              //  }
+            });
+
+            Thread thread2 = new Thread(() -> {
+              // synchronized (vector) {
+                   for (int i = 0; i < vector.size(); i++) {
+                       System.out.println(vector.get(i));
+                   }
+              // }
+            });
+
+            thread1.start();
+            thread2.start();
+
+        }
+    }
+}

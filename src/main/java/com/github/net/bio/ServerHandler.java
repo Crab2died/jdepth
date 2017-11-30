5b15ddf78808850d3369d49687a9b35541c597f1
Crab2Died <hwb9588@126.com>
2017-11-30 17:24:31 +0800

    nio

---
+++ src/main/java/com/github/net/bio/ServerHandler.java
@@ -0,0 +1,40 @@
+package com.github.net.bio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerHandler implements Runnable {
+
+    private Socket socket;
+
+    public ServerHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(this.socket.getInputStream())
+            );
+                 PrintWriter pw = new PrintWriter(this.socket.getOutputStream(), true)
+            ) {
+                for (; ; ) {
+                    String line = br.readLine();
+                    if (line != null) {
+                        System.out.println("request is :" + line);
+                        pw.println(System.currentTimeMillis() + ":" + line);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            socket = null;
+            Thread.interrupted();
+        }
+    }
+
+}
---
+++ src/test/java/com/github/crab2died/utils/ReflexTest.groovy
@@ -0,0 +1,11 @@
+package com.github.crab2died.utils
+
+class ReflexTest{
+
+    static void main(def args){
+        println "Aa" == "BB"
+        println "Aa".hashCode()
+        println "BB".hashCode()
+        println "Hello!"
+    }
+}

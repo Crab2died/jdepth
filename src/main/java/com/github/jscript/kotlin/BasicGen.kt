81220612668cdd66d26ff8a35e0a20124028ec4d
Crab2Died <hwb9588@126.com>
2017-12-27 17:30:02 +0800

    jscript

--- src/main/java/com/github/jscript/kotlin/BasicGen.kt
+++ src/main/java/com/github/jscript/kotlin/BasicGen.kt
@@ -4,6 +4,8 @@ fun main(ags: Array<String>) {
     println("Hello World")
 
     basicDataType()
+
+    condition(2)
 }
 
 fun basicDataType() {
@@ -21,3 +23,27 @@ fun basicDataType() {
 
 }
 
+fun condition(what: Int) {
+
+    var a = what
+
+    // 条件
+    if (a in 1..5)
+        println("$a in [1,5]")
+    else
+        print("$a not in [1,5]")
+
+    // 类三目
+    a = if (a > 0) 8 else 0
+
+    println(a)
+
+    // 类switch
+    when (a){
+        1 -> println("$a")
+        in 1..8 -> print("$a in [1,8]")
+        else -> {
+            println("I do not known")
+        }
+    }
+}

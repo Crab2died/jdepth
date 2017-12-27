3f647e6c2005b5045a1bb43afc6d367b733790d8
Crab2Died <hwb9588@126.com>
2017-12-27 16:07:25 +0800

    jscript

--- src/main/java/com/github/jscript/kotlin/BasicGen.kt
+++ src/main/java/com/github/jscript/kotlin/BasicGen.kt
@@ -1,7 +1,23 @@
 package com.github.jscript.kotlin
 
 fun main(ags: Array<String>) {
-    print("Hello World")
+    println("Hello World")
+
+    basicDataType()
 }
 
+fun basicDataType() {
+
+    val a: Char = 'a'
+    val b: Byte = 127
+    val c: Short = 12
+    val d: Int = 7811
+    val e: Double = 123.1
+    val f: Long = 121212L
+    val g: Boolean = true
+
+    println(a.toDouble() + b + c + d + e + f)
+    println(g)
+
+}
 

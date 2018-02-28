bf64990001f70f64cd706cf4966e037f8add7612
Crab2Died <hwb9588@126.com>
2018-02-28 13:54:32 +0800

    ipc

--- src/main/java/com/github/usefultool/ipc/SignalListener.java
+++ src/main/java/com/github/usefultool/ipc/SignalListener.java
@@ -17,50 +17,46 @@ public class SignalListener {
     public static void onListen(SignalEvent event) {
 
         // 独立线程监听
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                FileChannel fc = null;
-                try {
-                    File file = new File(".lock");
-                    if (!file.exists())
-                        if (!file.createNewFile())
-                            throw new IOException("can not create the .lock file");
-                    RandomAccessFile raf = new RandomAccessFile(".lock", "rw");
-                    fc = raf.getChannel();
-                    MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_WRITE, 0, 16);
-                    for (int i = 0; i < 16; i++)
-                        mbb.put(i, (byte) 0);
-                    for (; ; ) {
-                        int sign = mbb.get(0);
-                        if (sign == Signal.STOP) {
-                            mbb.put(0, (byte) 0);
-                            event.stop();
-                            break;
-                        }
-                        if (sign == Signal.RESTART) {
-                            mbb.put(0, (byte) 0);
-                            event.restart();
-                        }
-                        if (sign == Signal.STATUS) {
-                            mbb.put(0, (byte) 0);
-                            event.status();
-                        }
+        new Thread(() -> {
+            FileChannel fc = null;
+            try {
+                File file = new File(".lock");
+                if (!file.exists())
+                    if (!file.createNewFile())
+                        throw new IOException("can not create the .lock file");
+                RandomAccessFile raf = new RandomAccessFile(".lock", "rw");
+                fc = raf.getChannel();
+                MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_WRITE, 0, 16);
+                for (int i = 0; i < 16; i++)
+                    mbb.put(i, (byte) 0);
+                for (; ; ) {
+                    int sign = mbb.get(0);
+                    if (sign == Signal.STOP) {
+                        mbb.put(0, (byte) 0);
+                        event.stop();
+                        break;
                     }
-                } catch (IOException e) {
-                    System.err.println("lock file channel create error");
-                    System.exit(2);
-                } finally {
-                    if (null != fc) {
-                        try {
-                            fc.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                    if (sign == Signal.RESTART) {
+                        mbb.put(0, (byte) 0);
+                        event.restart();
                     }
-
+                    if (sign == Signal.STATUS) {
+                        mbb.put(0, (byte) 0);
+                        event.status();
+                    }
                 }
+            } catch (IOException e) {
+                System.err.println("lock file channel create error");
+                System.exit(2);
+            } finally {
+                if (null != fc) {
+                    try {
+                        fc.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             }
         }).start();
     }

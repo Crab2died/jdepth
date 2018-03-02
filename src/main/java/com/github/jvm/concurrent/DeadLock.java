e28bea93e6ee84f57d2be879d351767f242bf709
Crab2Died <hwb9588@126.com>
2018-03-02 16:02:00 +0800

    juc

---
+++ src/main/java/com/github/jvm/concurrent/DeadLock.java
@@ -0,0 +1,52 @@
+package com.github.jvm.concurrent;
+
+/**
+ * 死锁例子
+ */
+public class DeadLock {
+
+    /**
+     * A锁
+     */
+    private static String A = "A";
+    /**
+     * B锁
+     */
+    private static String B = "B";
+
+    public static void main(String[] args) {
+        new DeadLock().deadLock();
+    }
+
+    private void deadLock() {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (A) {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (B) {
+                        System.out.println("1");
+                    }
+                }
+            }
+        }, "dead-thread-01");
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (B) {
+                    synchronized (A) {
+                        System.out.println("2");
+                    }
+                }
+            }
+        }, "dead-thread-02");
+        t1.start();
+        t2.start();
+    }
+
+}

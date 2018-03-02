39e8314948fff8f29980f287db2b7a7a9ff65213
Crab2Died <hwb9588@126.com>
2018-03-02 10:41:22 +0800

    jline

--- src/main/java/com/github/usefultool/JLineSample.java
+++ src/main/java/com/github/usefultool/JLineSample.java
@@ -9,15 +9,16 @@ import java.util.List;
 public class JLineSample {
     public static void main(String... args) throws IOException {
 
-        //System.setProperty("jline.WindowsTerminal.directConsole", "false");
-        //System.setProperty("jline.internal.Log.debug", "true");
+        // IDEA、Eclipse调试时使用
+//        System.setProperty("jline.WindowsTerminal.directConsole", "false");
+//        System.setProperty("jline.internal.Log.debug", "true");
         ConsoleReader reader = new ConsoleReader();
         reader.addCompleter(new Completer() {
             @Override
             public int complete(String buffer, int cursor, List<CharSequence> candidates) {
                 if (buffer.length() > 0 && cursor > 0) {
                     String prefix = buffer.substring(cursor);
-                    if (prefix.startsWith("m")) {
+                    if (prefix.endsWith("m")) {
                         candidates.add("mdir");
                         candidates.add("mfile");
                         candidates.add("make");
@@ -31,5 +32,7 @@ public class JLineSample {
             cmd = reader.readLine("JLine >");
             System.out.println(cmd);
         } while (cmd != null && !"exit".equals(cmd));
+
+        reader.close();
     }
 }

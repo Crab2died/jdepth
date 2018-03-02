850ce8ccd6eff3fd442ec179287972561160319c
Crab2Died <hwb9588@126.com>
2018-03-02 10:32:25 +0800

    jline

---
+++ src/main/java/com/github/usefultool/JLineSample.java
@@ -0,0 +1,35 @@
+package com.github.usefultool;
+
+import jline.console.ConsoleReader;
+import jline.console.completer.Completer;
+
+import java.io.IOException;
+import java.util.List;
+
+public class JLineSample {
+    public static void main(String... args) throws IOException {
+
+        //System.setProperty("jline.WindowsTerminal.directConsole", "false");
+        //System.setProperty("jline.internal.Log.debug", "true");
+        ConsoleReader reader = new ConsoleReader();
+        reader.addCompleter(new Completer() {
+            @Override
+            public int complete(String buffer, int cursor, List<CharSequence> candidates) {
+                if (buffer.length() > 0 && cursor > 0) {
+                    String prefix = buffer.substring(cursor);
+                    if (prefix.startsWith("m")) {
+                        candidates.add("mdir");
+                        candidates.add("mfile");
+                        candidates.add("make");
+                    }
+                }
+                return cursor - 2;
+            }
+        });
+        String cmd;
+        do {
+            cmd = reader.readLine("JLine >");
+            System.out.println(cmd);
+        } while (cmd != null && !"exit".equals(cmd));
+    }
+}

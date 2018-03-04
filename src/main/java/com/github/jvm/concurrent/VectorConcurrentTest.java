5d2b060b834e4286ee7cdee4668d882aafbb8a43
HeWbing <hwb9588@126.com>
2018-03-04 22:51:09 +0800

    JUC

--- src/main/java/com/github/jvm/concurrent/VectorConcurrentTest.java
+++ src/main/java/com/github/jvm/concurrent/VectorConcurrentTest.java
@@ -3,32 +3,31 @@ package com.github.jvm.concurrent;
 import java.util.Vector;
 
 /**
- *  Vector 非绝对线程安全
- *
+ * Vector 非绝对线程安全
  */
 public class VectorConcurrentTest {
 
     private static Vector<Integer> vector = new Vector<>();
 
-    public static void main(String[] args){
-        for (;;){
-            for(int i = 0; i < 10; i++){
+    public static void main(String[] args) {
+        for (; ; ) {
+            for (int i = 0; i < 10; i++) {
                 vector.add(i);
             }
             Thread thread1 = new Thread(() -> {
-               // synchronized (vector){
-                    for(int i = 0; i < vector.size(); i++){
-                        vector.remove(i);
-                    }
-              //  }
+                // synchronized (vector){
+                for (int i = 0; i < vector.size(); i++) {
+                    vector.remove(i);
+                }
+                //  }
             });
 
             Thread thread2 = new Thread(() -> {
-              // synchronized (vector) {
-                   for (int i = 0; i < vector.size(); i++) {
-                       System.out.println(vector.get(i));
-                   }
-              // }
+                // synchronized (vector) {
+                for (int i = 0; i < vector.size(); i++) {
+                    System.out.println(vector.get(i));
+                }
+                // }
             });
 
             thread1.start();

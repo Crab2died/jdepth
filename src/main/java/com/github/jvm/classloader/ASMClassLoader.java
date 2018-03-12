44597d71058532cc4ce55b2d81aa5aaa5a3325a0
Crab2Died <hwb9588@126.com>
2018-03-12 15:18:56 +0800

    asm class loader

--- src/main/java/com/github/jvm/classloader/ASMClassLoader.java
+++ src/main/java/com/github/jvm/classloader/ASMClassLoader.java
@@ -1,10 +1,16 @@
 package com.github.jvm.classloader;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 // asm 的ClassLoader
 public class ASMClassLoader extends ClassLoader {
 
     // class字节码数组
     private byte[] clazzBytes;
+
+    // 缓存
+    private static Map<String, Class<?>> CACHE_CLASS = new ConcurrentHashMap<>();
 
     public ASMClassLoader(byte[] clazzBytes) {
         super();
@@ -13,21 +19,22 @@ public class ASMClassLoader extends ClassLoader {
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        Class<?> clazz;
-        try {
-            // 委派父加载器
-            clazz = getParent().loadClass(name);
-        } catch (ClassNotFoundException e) {
-            // 父加载器无法加载再调用自定义类加载器
-            clazz = findClass(name);
-        }
+
+        Class<?> clazz = findClass(name);
+        if (null == clazz)
+            try {
+                // 委派父加载器
+                clazz = getParent().loadClass(name);
+            } catch (ClassNotFoundException e) {
+                // 父加载器无法加载再调用自定义类加载器
+                clazz = defineClass(name, clazzBytes, 0, clazzBytes.length);
+                CACHE_CLASS.put(name, clazz);
+            }
         return clazz;
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        // 加载类字节码
-        return defineClass(name, clazzBytes, 0, clazzBytes.length);
+        return CACHE_CLASS.get(name);
     }
-
 }

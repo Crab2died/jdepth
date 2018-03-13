bb1c197ceacb028416ec1c639ea0175b06b0a547
Crab2Died <hwb9588@126.com>
2018-03-13 17:11:36 +0800

    asm class loader

--- src/main/java/com/github/jvm/classloader/ASMClassLoader.java
+++ src/main/java/com/github/jvm/classloader/ASMClassLoader.java
@@ -16,25 +16,31 @@ public class ASMClassLoader extends ClassLoader {
         super();
         this.clazzBytes = clazzBytes;
     }
-
-    @Override
-    public Class<?> loadClass(String name) throws ClassNotFoundException {
-
-        Class<?> clazz = findClass(name);
-        if (null == clazz)
-            try {
-                // 委派父加载器
-                clazz = getParent().loadClass(name);
-            } catch (ClassNotFoundException e) {
-                // 父加载器无法加载再调用自定义类加载器
-                clazz = defineClass(name, clazzBytes, 0, clazzBytes.length);
-                CACHE_CLASS.put(name, clazz);
-            }
-        return clazz;
-    }
+//
+//    // 并发同步??
+//    @Override
+//    public Class<?> loadClass(String name) throws ClassNotFoundException {
+//
+//        Class<?> clazz = findClass(name);
+//        if (null == clazz)
+//            try {
+//                // 委派父加载器
+//                clazz = getParent().loadClass(name);
+//            } catch (ClassNotFoundException e) {
+//                // 父加载器无法加载再调用自定义类加载器
+//                clazz = defineClass(name, clazzBytes, 0, clazzBytes.length);
+//                CACHE_CLASS.put(name, clazz);
+//            }
+//        return clazz;
+//    }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        return CACHE_CLASS.get(name);
+        Class<?> clazz = CACHE_CLASS.get(name);
+        if (null != clazz)
+            return clazz;
+        clazz = defineClass(name, clazzBytes, 0, clazzBytes.length);
+        CACHE_CLASS.put(name, clazz);
+        return clazz;
     }
 }

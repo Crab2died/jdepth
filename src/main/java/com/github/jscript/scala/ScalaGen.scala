91ba85d63fc265ce3cae2b83ec987cadb1cda4a7
HeWbing <hwb9588@126.com>
2018-09-28 22:24:13 +0800

    scala

--- src/main/java/com/github/jscript/scala/ScalaGen.scala
+++ src/main/java/com/github/jscript/scala/ScalaGen.scala
@@ -3,7 +3,24 @@ package com.github.jscript.scala
 object ScalaGen {
 
   def main(args: Array[String]): Unit = {
-    print("hello world")
+
+    println("hello world")
+
+    for (i <- 1 to 5 by 1 if i % 2 == 0) {
+      println(i)
+    }
+
+    // 推导式
+    val v = for (i <- 1 to 5 by 2) yield {
+      println(i); i
+    }
+    println(v)
+
+
+    // list
+    var list1 = List("A", "B", "C")
+    val list2 = "E":: list1
+    println(list1 :: list2)
   }
 
 }
